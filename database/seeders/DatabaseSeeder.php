b511bd1fe9270b5360282bb7fb33d8859832991a
Sondos <sondosahmedabed@gmail.com>
2023-09-11 23:34:45 +0300

    Unverified Cases Seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         // To generate 10 users data on top of the existing data
         \App\Models\User::factory(10)->create();
-        \App\Models\Task::factory(20)->create();        
+        // mixed values of the models use unverfied
+        \App\Models\User::factory(2)->unverified()->create();
+        \App\Models\Task::factory(20)->create();     
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
