09978da96b3097a88f796c5df451cb437dd48f77
Sondos <sondosahmedabed@gmail.com>
2023-09-12 16:34:38 +0300

    Mass assignmnet using fillable

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    /*
+    For securoty reasons this is called mass assignment
+    */
+    // using fillabee is more secured than using guareded
+    // becaue any new feild will be considered as not guarded
+    protected $fillable = ['title', 'description','long_description'];
+    // the opposite of fillable
+    //protected $guarded = ['secret'];
 }
