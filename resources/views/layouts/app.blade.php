5eea7578219da2957822b88f74f3dfe7f1d3abf2
Sondos <sondosahmedabed@gmail.com>
2023-09-15 00:20:40 +0300

    Using Alpine.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
 
 <head>
     <title>Tasks List App</title>
-      <script src="https://cdn.tailwindcss.com"></script>
-
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
     {{-- blade-formatter-disable --}}
       <style type="text/tailwindcss">
         .btn {
@@ -36,15 +36,24 @@
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
 {{-- we use yeild to fix the problem whhere to render this --}}
     <h1 class="text-3xl mb-4">@yield('title')</h1>
-    <div>
+    <div x-data="{ flash: true }">
     {{-- this is a directive  --}}
         @if(session()->has('success'))
-            <div class="mb-10 rounded border border-green-400 bg-green-100 px-4 py-3">
-            <strong class="font-bold">{{ session('success') }}</strong>
-            <div></div>
-            </div>
+            <div x-show="flash" 
+            class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+            role="alert">
+            <strong class="font-bold">Success!</strong>
+        <div>{{ session('success') }}</div>
+          <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+            stroke-width="1.5" @click="flash = false"
+            stroke="currentColor" class="h-6 w-6 cursor-pointer">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </span>
+        </div>
         @endif
-        @yield('content')
+      @yield('content')
     </div>
 </body>
 </html>
